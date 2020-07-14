f36b841cb407b3b51a7ab874d74f6404d93a76c4
miroslav.hynst <miroslav.hynst@oxyshop.cz>
2020-07-14 17:50:42 +0200

    Initial commit

---
+++ src/main/java/cz/mira/myapps/mydebt/MydebtApplication.java
@@ -0,0 +1,13 @@
+package cz.mira.myapps.mydebt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MydebtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MydebtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cz/mira/myapps/mydebt/MydebtApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.mira.myapps.mydebt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MydebtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
