618b1d1d249d8d100fdd851dd9878cd307274bae
miroslav.hynst <miroslav.hynst@oxyshop.cz>
2020-07-17 11:05:01 +0200

    added localization

---
+++ web/vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "transpileDependencies": [
+    "vuetify"
+  ],
+
+  pluginOptions: {
+    i18n: {
+      locale: 'en',
+      fallbackLocale: 'cz',
+      localeDir: 'locales',
+      enableInSFC: true
+    }
+  }
+}
