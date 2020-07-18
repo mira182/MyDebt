d86afde3ed2c1238298863ee020783dbbcbcaf60
miroslav.hynst <miroslav.hynst@oxyshop.cz>
2020-07-18 16:15:15 +0200

    allowed all on spring security

--- src/main/java/cz/mira/myapps/mydebt/config/SecurityConfig.java
+++ src/main/java/cz/mira/myapps/mydebt/config/SecurityConfig.java
@@ -13,7 +13,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.authorizeRequests().antMatchers("/").permitAll();
+        httpSecurity.authorizeRequests().antMatchers("/**").permitAll();
     }
 
 
